5f344cca9b7860b900de8917cf650b573affc4a9
rasikasamith <rasikasamith@gmail.com>
2022-05-07 22:35:56 +0530

    upto 22 changes

---
+++ eTickets/Controllers/ActorsController.cs
@@ -0,0 +1,25 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ActorsController(AppDbContext context)
+        {
+            _dbContext = context;
+        }
+        public IActionResult Index()
+        {          
+
+            var data = _dbContext.Actors.ToList();
+            return View(data);
+        }
+    }
+}
---
+++ eTickets/Data/Enums/MovieCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comody = 2,
+        Drama  =3,
+        Documentary=4,
+        Cartoon=5,
+        Horror=6
+    }
+}
