3ddf5b762cae4c18446c64a41d11ebd59780c14b
rasikasamith <rasikasamith@gmail.com>
2022-05-26 09:28:40 +0530

    upto step 72

---
+++ eTickets/Controllers/OrdersController.cs
@@ -0,0 +1,58 @@
+﻿using eTickets.Data.Cart;
+using eTickets.Data.Services;
+using eTickets.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IMoviesService _moviesService;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrdersController(IMoviesService moviesService, ShoppingCart shoppingCart)
+        {
+            _moviesService = moviesService;
+            _shoppingCart = shoppingCart;
+        }
+        public IActionResult ShoppingCart()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+            var responce = new ShoppingCartVM()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal= _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(responce);
+        }
+
+        public async Task<RedirectToActionResult> AddItemToShoppigCart(int id)
+        {
+            var item = await _moviesService.GetMovieByIdAsync(id);
+
+            if(item!=null)
+            {
+                _shoppingCart.AddItemtoCart(item);
+
+            }
+            return RedirectToAction(nameof(ShoppingCart));
+        }
+
+        public async Task<RedirectToActionResult> RemoveItemFromShoppigCart(int id)
+        {
+            var item = await _moviesService.GetMovieByIdAsync(id);
+
+            if (item != null)
+            {
+                _shoppingCart.RemoveItemFromCart(item);
+
+            }
+            return RedirectToAction(nameof(ShoppingCart));
+        }
+    }
+}
--- eTickets/Data/Cart/ShoppingCart.cs
+++ eTickets/Data/Cart/ShoppingCart.cs
@@ -1,5 +1,7 @@
 ﻿using eTickets.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +18,55 @@ namespace eTickets.Data.Cart
         public ShoppingCart(AppDbContext appDbContext)
         {
             _appDbContext = appDbContext;
+        }
+
+        public static ShoppingCart GetShoppingCart(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+            var context = services.GetService<AppDbContext>();
+
+            string cartId = session.GetString("CartId") ?? Guid.NewGuid().ToString();
+            session.SetString("CartId", cartId);
+
+            return new ShoppingCart(context) { ShoppingCartId = cartId };
+        }
+        public void AddItemtoCart(Movie movie)
+        {
+            var shoppingCartitem = _appDbContext.ShoppingCartItems.FirstOrDefault(n => n.Movie.Id == movie.Id && n.ShoppingCartId == ShoppingCartId);
+            if(shoppingCartitem==null)
+            {
+                shoppingCartitem = new ShoppingCartItem()
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Movie = movie,
+                    Amount = 1
+                };
+                
+                _appDbContext.ShoppingCartItems.Add(shoppingCartitem);
+            }
+            else
+            {
+                shoppingCartitem.Amount++;
+            }
+            _appDbContext.SaveChanges();
+        }
+
+        public void RemoveItemFromCart(Movie movie)
+        {
+            var shoppingCartitem = _appDbContext.ShoppingCartItems.FirstOrDefault(n => n.Movie.Id == movie.Id && n.ShoppingCartId == ShoppingCartId);
+            if (shoppingCartitem != null)
+            {
+                if (shoppingCartitem.Amount > 1)
+                {
+                    shoppingCartitem.Amount--;
+                }
+                else
+                {
+                    _appDbContext.ShoppingCartItems.Remove(shoppingCartitem);
+                }
+            }
+
+            _appDbContext.SaveChanges();
         }
 
         public List<ShoppingCartItem> GetShoppingCartItems()
